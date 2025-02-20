e73172233b5d564746280f96eb6663e0d835d90f
PyhtonDeveloperNishu <yashkumar5147@gmail.com>
2025-02-21 03:00:43 +0530

    dom_1 added style by js to html

---
+++ dom.js
@@ -0,0 +1,32 @@
+/*1.Use the getElementById method to retrieve the target div element.
+ Properly store the fetched element in a variable for further manipulation*/
+let con = document.getElementById("container");
+let h1 =  document.getElementsByTagName("h1");
+
+/* Change the background color of the fetched div element to yellow 
+using the style.backgroundColor property.*/
+con.style.backgroundColor="yellow";
+
+/* Add a margin of 20 pixels to the element using the 
+style.margin property.*/
+con.style.margin="20px";
+
+/* Apply a padding of 10 pixels using the style.padding property*/
+con.style.padding="10px";
+
+/*Change the font size to 18 pixels using the style.fontSize property.*/
+con.style.fontSize="18px";
+
+/*Set the font weight to bold using the style.fontWeight property. */
+con.style.fontWeight="bold";
+
+/*Change the height of the element to 200 pixels using the style.height property*/
+con.style.height="200px";
+
+/*Modify the width of the element to 300 pixels using the style.width property */
+con.style.width="300px";
+
+con.style.color="blue";
+
+
+
